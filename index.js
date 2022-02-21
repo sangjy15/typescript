16044e02acd62277b6585eddefc6208002a9d06b
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2022-02-21 21:48:53 +0900

    types

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "amy", age = 33, gender = "female";
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-};
-sayHi(name, age, gender);
-//# sourceMappingURL=index.js.map
