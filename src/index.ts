0921abeceed2d36135c6d90164ac7986cd0b0bf0
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2022-02-22 22:19:15 +0900

    interface

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-const sayHi = (name:string, age:number, gender:string):string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+interface Human {
+    name:string;
+    age:number;
+    gender: string;
 }
 
-console.log(sayHi("Amy", 33, "male"));
+const person = {
+    name: "amy",
+    age: 22,
+    gender: "female"
+}
+
+const sayHi = (person: Human):string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+}
+
+console.log(sayHi(person));
 
 export{};
