e117fd5b844f5cbf7efa6ae2986968978d160aac
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2022-02-24 22:02:17 +0900

    block part

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import * as CryptosJS from "crypto-js";
+
 class Block{
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (
+        index:number, 
+        previousHash:string, 
+        timestamp:number, 
+        data:string):string => 
+        CryptosJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(
         index: number,
         hash: string,
@@ -20,8 +30,13 @@ class Block{
 
 const genesisBlock:Block = new Block(0, "124576234", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
+const getBlockchain = () :Block[] => blockchain;
+
+const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp =(): number => Math.round(new Date().getTime() / 1000);
 
 console.log(genesisBlock);
 export{};
